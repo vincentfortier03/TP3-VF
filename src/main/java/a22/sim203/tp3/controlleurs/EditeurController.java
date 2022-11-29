b3c50ab28df98bc5f4895eead7729e2714c40810
vincentfortier03 <89474285+vincentfortier03@users.noreply.github.com>
2022-11-29 14:31:47 -0500

    commencé a faire les list views de lediteur

--- src/main/java/a22/sim203/tp3/controlleurs/EditeurController.java
+++ src/main/java/a22/sim203/tp3/controlleurs/EditeurController.java
@@ -1,9 +1,6 @@
 package a22.sim203.tp3.controlleurs;
 
-import a22.sim203.tp3.simulation.Equation;
-import a22.sim203.tp3.simulation.Etat;
-import a22.sim203.tp3.simulation.Simulation;
-import a22.sim203.tp3.simulation.Variable;
+import a22.sim203.tp3.simulation.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -27,11 +24,42 @@ public class EditeurController implements Initializable {
     @FXML
     private BorderPane root;
 
+    private Simulation currentSim;
+
+    public Etat etatTest;
+
+    public SimulationService simService;
+
+    public Equation equation;
+
+    public Etat lastEtat;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        equation = new Equation("test", "f(x)=x+1");
+        etatTest = new Etat();
+        etatTest.addVariable(new Variable("x", 20));
+        etatTest.getVariable("x").ajouteEquation(equation);
+
+
+        simService = new SimulationService("test", etatTest);
+        simService.setTempsEtIntervalTheorique(0,1);
         listViewEquations = new ListView<>();
         listViewSimulations = new ListView<>();
         listViewSimulations = new ListView<>();
+
+        listViewSimulations.getItems().add(simService.getSimulation());
+
+
+        listViewSimulations.selectionModelProperty().addListener((i) -> {
+            listViewVariables.getItems().addAll(listViewSimulations.getSelectionModel().getSelectedItem().getHistorique(listViewSimulations.getSelectionModel().getSelectedItem().getHistorique().size()-1).getVariableList());
+        });
+
+        listViewVariables.selectionModelProperty().addListener((o) -> {
+            listViewEquations.getItems().addAll(listViewVariables.getSelectionModel().getSelectedItem().getEquationsCollection());
+        });
 
 
 
